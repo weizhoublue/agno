22d93b7e972dea7b5eef19def6236d41691eeac5
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2025-08-25 20:55:05 +0800

    feat: integrate Ollama model and embedder with PDF knowledge base setup

--- cookbook/agent_concepts/knowledge/README.md
+++ cookbook/agent_concepts/knowledge/README.md
@@ -1,33 +1,16 @@
 # Agent Knowledge
-
-**Knowledge Base:** is information that the Agent can search to improve its responses. This directory contains a series of cookbooks that demonstrate how to build a knowledge base for the Agent.
-
-> Note: Fork and clone this repository if needed
-
+ 
 ### 1. Create a virtual environment
 
 ```shell
-python3 -m venv ~/.venvs/aienv
-source ~/.venvs/aienv/bin/activate
-```
+uv venv
+export UV_INDEX_URL=https://mirrors.aliyun.com/pypi/simple/
+uv pip install -U pgvector "psycopg[binary]" sqlalchemy openai agno pypdf bs4
 
-### 2. Install libraries
-
-```shell
-pip install -U pgvector "psycopg[binary]" sqlalchemy openai agno
-```
+ 
+``` 
 
 ### 3. Run PgVector
-
-> Install [docker desktop](https://docs.docker.com/desktop/install/mac-install/) first.
-
-- Run using a helper script
-
-```shell
-./cookbook/scripts/run_pgvector.sh
-```
-
-- OR run using the docker run command
 
 ```shell
 docker run -d \
@@ -44,12 +27,6 @@ docker run -d \
 ### 4. Test Knowledge Cookbooks
 
 Eg: PDF URL Knowledge Base
-
-- Install libraries
-
-```shell
-pip install -U pypdf bs4
-```
 
 - Run the PDF URL script
 
--- cookbook/agent_concepts/knowledge/pdf_url_kb.py
+++ cookbook/agent_concepts/knowledge/pdf_url_kb.py
@@ -1,16 +1,37 @@
+# docker run -d \
+#   -e POSTGRES_DB=ai \
+#   -e POSTGRES_USER=ai \
+#   -e POSTGRES_PASSWORD=ai \
+#   -e PGDATA=/var/lib/postgresql/data/pgdata \
+#   -v pgvolume:/var/lib/postgresql/data \
+#   -p 5532:5432 \
+#   --name pgvector \
+#   agnohq/pgvector:16
+
 from agno.agent import Agent
 from agno.knowledge.pdf_url import PDFUrlKnowledgeBase
 from agno.vectordb.pgvector import PgVector
+from agno.models.ollama import Ollama
+from agno.embedder.ollama import OllamaEmbedder
 
-db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+db_url = "postgresql+psycopg://ai:ai@10.20.1.60:5532/ai"
+pdf_url = "https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"
 
 knowledge_base = PDFUrlKnowledgeBase(
-    urls=["https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
-    vector_db=PgVector(table_name="recipes", db_url=db_url),
+    # 通过在线的 url 获取 pdf 文件
+    urls=[pdf_url],
+    vector_db=PgVector(
+            table_name="recipes", 
+            db_url=db_url,
+            embedder=OllamaEmbedder(id="nomic-embed-text:latest", dimensions=768 , host="http://10.20.1.60:11434" ),
+        ),
 )
-knowledge_base.load(recreate=False)  # Comment out after first run
+# 如果之前创建过向量数据库的同名 table ， embedder dimensions 和同其匹配。否则，可以设置 recreate=True 来重新创建
+knowledge_base.load(recreate=True)  # Comment out after first run
 
 agent = Agent(
+    # 模型要支持 tool
+    model=Ollama(id="llama3.1:latest",host="http://10.20.1.60:11434"),
     knowledge=knowledge_base,
     search_knowledge=True,
 )
