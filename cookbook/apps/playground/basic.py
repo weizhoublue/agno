a65efc81ea982555fadfc7b799af43335701c252
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2025-09-01 20:58:06 +0800

    refactor: switch from OpenAI to Ollama model and simplify agent configuration

--- cookbook/apps/playground/basic.py
+++ cookbook/apps/playground/basic.py
@@ -1,39 +1,20 @@
 from agno.agent import Agent
 from agno.memory.agent import AgentMemory
 from agno.memory.db.postgres import PgMemoryDb
-from agno.models.openai import OpenAIChat
+from agno.models.ollama import Ollama
 from agno.playground import Playground, serve_playground_app
 from agno.storage.agent.sqlite import SqliteAgentStorage
 from agno.storage.postgres import PostgresStorage
 
-agent_storage_file: str = "tmp/agents.db"
-
-db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 basic_agent = Agent(
     name="Basic Agent",
-    model=OpenAIChat(id="gpt-4o"),
-    memory=AgentMemory(
-        db=PgMemoryDb(
-            table_name="agent_memory",
-            db_url=db_url,
-        ),
-        # Create and store personalized memories for this user
-        create_user_memories=True,
-        # Update memories for the user after each run
-        update_user_memories_after_run=True,
-        # Create and store session summaries
-        create_session_summary=True,
-        # Update session summaries after each run
-        update_session_summary_after_run=True,
-    ),
-    storage=PostgresStorage(
-        table_name="agent_sessions", db_url=db_url, auto_upgrade_schema=True
-    ),
+    model=Ollama(id="qwen3:8b",host="http://10.20.1.60:11434"),
     add_history_to_messages=True,
     num_history_responses=3,
     add_datetime_to_instructions=True,
     markdown=True,
+    debug_mode=True,
 )
 
 playground = Playground(
@@ -44,7 +25,19 @@ playground = Playground(
     description="A playground for basic agent",
     app_id="basic-agent",
 )
-app = playground.get_app()
 
+
+#  发现 网站打开后 failed to load endpoint
+#app = playground.get_app()
+# 使用空前缀或指定前缀 成功打开访问。 否则发现 网站打开后 failed to load endpoint
+app = playground.get_app(prefix="")  
 if __name__ == "__main__":
-    playground.serve(app="basic:app", reload=True)
+    # Start the playground server
+    playground.serve(
+        # 是一个模块路径字符串，告诉 uvicorn 从哪里导入 FastAPI 应用实例
+        # 从 basic.py 中的变量 app 导入
+        app="basic:app",
+        host="localhost",
+        port=7777,
+        reload=True,
+    )
